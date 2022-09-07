b54391b1b029c67e539f834ce261349e4ed0fe97
Saebom Lee <littleciel@naver.com>
2022-09-07 22:51:04 +0900

    2022.9.7.(WED)

---
+++ java004/src/java004/Main_divisor.java
@@ -0,0 +1,31 @@
+package java004;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main_divisor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 1; i <= N; i++) {
+			if (N % i == 0) {
+				list.add(i);
+			}
+		}
+		if (list.size() >= K) {
+			System.out.println(list.get(K - 1));
+
+		} else {
+			System.out.println(0);
+		}
+
+	}
+}
